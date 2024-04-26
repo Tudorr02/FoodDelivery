2bbf12b877e85b48dc2b4007734b95332d34b2d8
Ilie Octavian Tudor <92024989+Tudorr02@users.noreply.github.com>
2024-04-27 02:24:36 +0300

    Bug Solved

--- src/Main.java
+++ src/Main.java
@@ -7,27 +7,16 @@ import org.w3c.dom.ls.LSOutput;
 
 import javax.xml.crypto.Data;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
 
-        DataStorageServices.getInstance();
-        System.out.println(DataStorageServices.getInstance().getShoppingCarts().size());
+        DataStorageServices.getInstance().initData();
 
-        DataStorageServices.getInstance().setObjects(List.of(DataStorageServices.getInstance().getUsers()));
-        DataConverter<Client> writer = new ClientConverter();
-        DataStorageServices.getInstance().appendCsv("res/CSV/Client_Data.csv",(DataConverter) writer);  // Correct use of generics
-
-
-
-
-
-
+        DataStorageServices.getInstance().getClients().add(new Client("Tudori","AUDI","tas","1231","C-100123","40178066688","Cluj-Napoca","vasile.nicolae@example.com"));
+        DataStorageServices.getInstance().writeCsv("res/CSV/Client_Data.csv",(DataConverter)new ClientConverter(),(List)DataStorageServices.getInstance().getClients());
     }
 
 }
--- src/com/FoodDeliveryApp/Services/DataStorageServices.java
+++ src/com/FoodDeliveryApp/Services/DataStorageServices.java
@@ -3,14 +3,10 @@ package com.FoodDeliveryApp.Services;
 import com.FoodDeliveryApp.Converters.*;
 
 import com.FoodDeliveryApp.Models.*;
-import org.w3c.dom.ls.LSOutput;
 
-import javax.xml.crypto.Data;
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,183 +14,117 @@ public class DataStorageServices<T> {
     // Static variable to hold the single instance
     private static DataStorageServices<?> instance;
 
-    // List to hold multiple objects of type T
-    private List<T> objects;
-
-    // users
-    private List<Users> users;
-    private List<Client> cUsers;
-    private List<DeliveryMan> dmUsers;
-
+    private List<Client> clients;
+    private List<DeliveryMan> deliveryMans;
     private List<FoodItem> foodItems;
     private List<Restaurant> restaurants;
     private List<Review> reviews;
+    private List<PickUpOrder> puOrders;
+    private List<DeliveryOrder> dOrders;
     private List<Delivery> deliveries;
     private List<ShoppingCart> shoppingCarts;
 
-    // orders
-    private List<PickUpOrder> puOrders;
-    private List<DeliveryOrder> dOrders;
-    private List<Order> orders;
-
-
-
-
-    // Private constructor to prevent external instantiation
-    private DataStorageServices() {
-        List<Client> cUsers=new ArrayList<>();
-        List<DeliveryMan> dmUsers = new ArrayList<>();
-        List<FoodItem> foodItems= new ArrayList<>();
-        List<Restaurant> restaurants= new ArrayList<>();
-        List<Review> reviews = new ArrayList<>();
-        List<PickUpOrder> puOrders = new ArrayList<>();
-        List<DeliveryOrder> dOrders = new ArrayList<>();
-        List<Delivery> deliveries = new ArrayList<>();
-        List<ShoppingCart> shoppingCarts = new ArrayList<>();
-        List<Order> orders = new ArrayList<>();
-        List<Users> users = new ArrayList<>();
-
-
-        objects = new ArrayList<>();
-    }
 
     // Generic method to return the single instance with appropriate type
-    public static synchronized <U> DataStorageServices<U> getInstance() {
+    public static synchronized<T>  DataStorageServices<T> getInstance() throws Exception {
         if (instance == null) {
-            instance = new DataStorageServices<U>();
+            instance = new DataStorageServices<T>();
 
-            instance.initializeData();
         }
-        return (DataStorageServices<U>) instance;
+        return (DataStorageServices) instance;
     }
 
 
-    private void initializeData(){
-         DataConverter<Client> clientConverter = new ClientConverter();
-         DataConverter<DeliveryMan> dmConverter = new DeliveryManConverter();
-         DataConverter<FoodItem> fiConverter = new FoodItemConverter();
-         DataConverter<Review> reviewConverter = new ReviewConverter();
-         DataConverter<Restaurant> restaurantConverter = new RestaurantConverter();
-         DataConverter<ShoppingCart> shoppingCartConverter= new ShoppingCartConverter();
-         DataConverter<PickUpOrder> puConverter = new PickUpOrdersConverter();
-         DataConverter<DeliveryOrder> doConverter = new DeliveryOrderConverter();
-         DataConverter<Delivery> deliveryDataConverter = new DeliveryConverter();
-
-
-
-        try {
-//            this.readCsv("res/CSV/Client_Data.csv", (DataConverter<T>) clientConverter);
-//            this.cUsers= (List<Client>) new ArrayList<>(this.objects);
-//            this.objects.clear();
+//    private void initializeData(){
+//         DataConverter<Client> clientConverter = new ClientConverter();
+//         DataConverter<DeliveryMan> dmConverter = new DeliveryManConverter();
+//         DataConverter<FoodItem> fiConverter = new FoodItemConverter();
+//         DataConverter<Review> reviewConverter = new ReviewConverter();
+//         DataConverter<Restaurant> restaurantConverter = new RestaurantConverter();
+//         DataConverter<ShoppingCart> shoppingCartConverter= new ShoppingCartConverter();
+//         DataConverter<PickUpOrder> puConverter = new PickUpOrdersConverter();
+//         DataConverter<DeliveryOrder> doConverter = new DeliveryOrderConverter();
+//         DataConverter<Delivery> deliveryDataConverter = new DeliveryConverter();
 //
-//            this.readCsv("res/CSV/DeliveryMen_Data.csv",(DataConverter<T>) dmConverter);
-//            this.dmUsers= (List<DeliveryMan>) new ArrayList<>(this.getObjects());
-//            objects.clear();
-
-            this.readCsv("res/CSV/Client_Data.csv", (DataConverter<T>) clientConverter);
-            this.readCsv("res/CSV/DeliveryMen_Data.csv", (DataConverter<T>) dmConverter);
-            this.users = (List<Users>) new ArrayList<>(this.objects);
-            this.objects.clear();
-
-            this.readCsv("res/CSV/FoodItems_Data.csv",(DataConverter<T>) fiConverter);
-            this.foodItems = (List<FoodItem>) new ArrayList<>(this.getObjects());
-            objects.clear();
-
-            this.readCsv("res/CSV/Reviews_Data.csv",(DataConverter<T>) reviewConverter);
-            this.reviews = (List<Review>) new ArrayList<>(this.getObjects());
-            objects.clear();
-
-            this.readCsv("res/CSV/Restaurants_Data.csv",(DataConverter<T>) restaurantConverter);
-            this.restaurants = (List<Restaurant>) new ArrayList<>(this.getObjects());
-            objects.clear();
-
-            this.readCsv("res/CSV/ShoppingCart_Data.csv",(DataConverter<T>) shoppingCartConverter);
-            this.shoppingCarts = (List<ShoppingCart>) new ArrayList<>(this.getObjects());
-            objects.clear();
-
-
-            this.readCsv("res/CSV/PickUpOrders_Data.csv",(DataConverter<T>) puConverter);
-            this.readCsv("res/CSV/DeliveryOrders.csv",(DataConverter<T>) doConverter);
-            this.orders = (List<Order>) new ArrayList<>(this.objects);
-            objects.clear();
-
-            this.readCsv("res/CSV/Delivery_Data.csv", (DataConverter<T>) deliveryDataConverter);
-            this.deliveries = (List<Delivery>) new ArrayList<>(this.objects);
-            objects.clear();
 //
-//            this.readCsv("res/CSV/FoodItems_Data.csv" , (DataConverter<T>) foodItems);
-//            this.foodItems= (List<FoodItem>) this.getObjects(); objects.clear();
 //
-//            this.readCsv("res/CSV/Restaurants_Data.csv", (DataConverter<T>) restaurants);
-//            this.restaurants= (List<Restaurant>) this.getObjects(); objects.clear();
-
-        } catch (IOException e) {
-            System.out.println("Error reading CSV data: " + e.getMessage());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        // Optionally, print out to verify loading
-
-//        System.out.println();
+//        try {
 //
-//        for( T DeliveryMan : this.getObjects()) {
-//            System.out.println(DeliveryMan.toString());
+////            this.readCsv("res/CSV/Client_Data.csv", (DataConverter<T>) clientConverter);
+////            this.cUsers= (List<Client>) new ArrayList<>(this.objects);
+////            this.objects.clear();
+////
+////            this.readCsv("res/CSV/DeliveryMen_Data.csv",(DataConverter<T>) dmConverter);
+////            this.dmUsers= (List<DeliveryMan>) new ArrayList<>(this.getObjects());
+////            objects.clear();
+////
+////            this.readCsv("res/CSV/Client_Data.csv", (DataConverter<T>) clientConverter);
+////            this.readCsv("res/CSV/DeliveryMen_Data.csv", (DataConverter<T>) dmConverter);
+////            this.users = (List<Users>) new ArrayList<>(this.objects);
+////            this.objects.clear();
+////
+////            this.readCsv("res/CSV/FoodItems_Data.csv",(DataConverter<T>) fiConverter);
+////            this.foodItems = (List<FoodItem>) new ArrayList<>(this.getObjects());
+////            objects.clear();
+////
+////            this.readCsv("res/CSV/Reviews_Data.csv",(DataConverter<T>) reviewConverter);
+////            this.reviews = (List<Review>) new ArrayList<>(this.getObjects());
+////            objects.clear();
+////
+////            this.readCsv("res/CSV/Restaurants_Data.csv",(DataConverter<T>) restaurantConverter);
+////            this.restaurants = (List<Restaurant>) new ArrayList<>(this.getObjects());
+////            objects.clear();
+////
+////            this.readCsv("res/CSV/ShoppingCart_Data.csv",(DataConverter<T>) shoppingCartConverter);
+////            this.shoppingCarts = (List<ShoppingCart>) new ArrayList<>(this.getObjects());
+////            objects.clear();
+////
+////
+////            this.readCsv("res/CSV/PickUpOrders_Data.csv",(DataConverter<T>) puConverter);
+////            this.readCsv("res/CSV/DeliveryOrders.csv",(DataConverter<T>) doConverter);
+////            this.orders = (List<Order>) new ArrayList<>(this.objects);
+////            objects.clear();
+////
+////            this.readCsv("res/CSV/Delivery_Data.csv", (DataConverter<T>) deliveryDataConverter);
+////            this.deliveries = (List<Delivery>) new ArrayList<>(this.objects);
+////            objects.clear();
+//
+//        } catch (IOException e) {
+//            System.out.println("Error reading CSV data: " + e.getMessage());
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
 //        }
+//
+//
+//    }
 
-    }
-
-    // Method to get the stored objects
-    public List<T> getObjects() {
-        return objects;
-    }
-
-    public List<Users> getUsers()
-    {
-        return users;
-    }
-
-    public List<FoodItem> getFoodItems() {
-        return foodItems;
-    }
-
-    public List<Restaurant> getRestaurants() {
-        return restaurants;
-    }
-
-    public List<Review> getReviews() {
-        return reviews;
-    }
-
-    public List<Delivery> getDeliveries() {
-        return deliveries;
-    }
-
-    public List<PickUpOrder> getPUOrders() {
-        return puOrders;
-    }
-
-    public List<DeliveryOrder> getDOrders() {
-        return dOrders;
-    }
-
-    public List<Order>getOrders(){
-        return orders;
-    }
-
-    public List<ShoppingCart> getShoppingCarts(){
-        return shoppingCarts;
-    }
+    public  void initData() throws Exception {
+        DataConverter<Client> clientConverter = new ClientConverter();
+        DataConverter<DeliveryMan> deliveryManConverter = new DeliveryManConverter();
+        DataConverter<FoodItem> foodItemConverter = new FoodItemConverter();
+        DataConverter<Restaurant> restaurantConverter = new RestaurantConverter();
+        DataConverter<Review> reviewConverter = new ReviewConverter();
+        DataConverter<PickUpOrder> puOrderDataConverter= new PickUpOrdersConverter();
+        DataConverter<DeliveryOrder> dOrderConverter = new DeliveryOrderConverter();
+        DataConverter<Delivery> deliveryConverter = new DeliveryConverter();
+        DataConverter<ShoppingCart> shoppingCartConverter = new ShoppingCartConverter();
 
 
+        clients= readCsv("res/CSV/Client_Data.csv",(DataConverter) clientConverter);
+        deliveryMans = readCsv("res/CSV/DeliveryMen_Data.csv",(DataConverter) deliveryManConverter);
+        foodItems=readCsv("res/CSV/FoodItems_Data.csv",(DataConverter) foodItemConverter );
+        reviews=readCsv("res/CSV/Reviews_Data.csv",(DataConverter)reviewConverter);
+        restaurants=readCsv("res/CSV/Restaurants_Data.csv",(DataConverter)restaurantConverter);
+        shoppingCarts=readCsv("res/CSV/ShoppingCart_Data.csv",(DataConverter)shoppingCartConverter);
+        puOrders=readCsv("res/CSV/PickUpOrders_Data.csv",(DataConverter)puOrderDataConverter);
+        dOrders=readCsv("res/CSV/DeliveryOrders.csv",(DataConverter)dOrderConverter);
+        deliveries=readCsv("res/CSV/Delivery_Data.csv",(DataConverter)deliveryConverter);
 
-    public void setObjects(List<T> objects) {
-
-        this.objects = new ArrayList<T>(objects);
     }
 
     // Method to read data from a CSV file
-    public void readCsv(String filePath, DataConverter<T> converter ) throws Exception {
+    public List<T> readCsv(String filePath, DataConverter<T> converter ) throws Exception {
+        List<T> objects = new ArrayList<>();
         try (BufferedReader br = Files.newBufferedReader(Paths.get(filePath))) {
             String line;
             int lineIndex = 0;
@@ -208,46 +138,79 @@ public class DataStorageServices<T> {
                 }
             }
         }
+
+        return objects;
     }
 
     // Method to write data to a CSV file
-    public void writeCsv(String filePath, DataConverter<T> converter) throws IOException {
-        try (BufferedWriter bw = Files.newBufferedWriter(Paths.get(filePath))) {
-            for (T object : objects) {
-                String csvLine = converter.convertToCsv(object);
-                bw.write(csvLine);
+    public void writeCsv(String filePath, DataConverter<T> converter , List<T> items ) throws IOException {
+            BufferedReader br = Files.newBufferedReader(Paths.get(filePath));
+            String val=br.readLine();
+            try(BufferedWriter bw = Files.newBufferedWriter(Paths.get(filePath))) {
+                bw.write(val);
                 bw.newLine();
+
+                for (T object : items) {
+
+                    String csvLine = converter.convertToCsv(object);
+                    bw.write(csvLine);
+                    bw.newLine();
+
+                }
             }
-        }
+
     }
 
-    public void appendCsv(String filePath, DataConverter<T> converter) throws IOException {
-        try (BufferedWriter bw = Files.newBufferedWriter(Paths.get(filePath),
-                                                     StandardCharsets.UTF_8,
-                                                     StandardOpenOption.CREATE,  // Create the file if it does not exist
-                                                     StandardOpenOption.APPEND)) {  // Append to the file if it exists
-
-        for (T object : objects) {
-            bw.newLine();
-            String csvLine = converter.convertToCsv(object);
-            bw.write(csvLine);
-
-        }
-    }
+    public List<Client> getClients() {
+        return clients;
     }
 
+    public List<ShoppingCart> getShoppingCarts() {
+        return shoppingCarts;
+    }
+
+    public List<Delivery> getDeliveries() {
+        return deliveries;
+    }
+
+    public List<DeliveryOrder> getdOrders() {
+        return dOrders;
+    }
+
+    public List<PickUpOrder> getPuOrders() {
+        return puOrders;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public List<Restaurant> getRestaurants() {
+        return restaurants;
+    }
+
+    public List<FoodItem> getFoodItems() {
+        return foodItems;
+    }
+
+    public List<DeliveryMan> getDeliveryMans() {
+        return deliveryMans;
+    }
 
     public Users getUserById (String userId) throws Exception {
 
-        if(this.users.isEmpty())
+        List<Users> users = new ArrayList<>();
+        users.addAll(clients);
+        users.addAll(deliveryMans);
+
+        if(users.isEmpty())
             throw new IOException("Custom Exception - getUserById : Empty Users List !");
 
-        for (Users user : this.users)
+        for (Users user : users)
             if(user.getUserID().equals(userId))
                 return user;
 
         throw new Exception("getUserById : User not found !");
-
 
     }
 
@@ -283,12 +246,12 @@ public class DataStorageServices<T> {
 
     public DeliveryMan getDeliveryManById(String deliveryManId) throws Exception {
 
-        if(this.users.isEmpty())
+        if(deliveryMans.isEmpty())
             throw new IOException("Custom Exception - getDeliveryManById : Empty DeliveryMan List !");
 
-        for (Users user : this.users)
-            if(user.getUserID().equals(deliveryManId) && user instanceof DeliveryMan)
-                return (DeliveryMan) user;
+        for (DeliveryMan dm : deliveryMans)
+            if(dm.getUserID().equals(deliveryManId))
+                return  dm;
 
         throw new Exception("getUserById : User not found !");
 
@@ -323,11 +286,16 @@ public class DataStorageServices<T> {
     }
 
     public Order getOrderById(String orderId) throws Exception {
-        if (this.orders.isEmpty()) {
+
+        List<Order> orders = new ArrayList<>();
+        orders.addAll(dOrders);
+        orders.addAll(puOrders);
+
+        if (orders.isEmpty()) {
             throw new IOException("Custom Exception - getOrderById : Empty Orders List !");
         }
 
-        for (Order order : this.orders) {
+        for (Order order : orders) {
             if (order.getOrderID().equals(orderId)) {
                 return order;
             }
